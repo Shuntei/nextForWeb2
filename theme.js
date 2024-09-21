05421cc21563f173d859f56221dcfe79d337a557
林俊廷 <ms0491826@gmail.com>
2024-09-21 13:12:48 +0800

    '09210112'

---
+++ theme.js
@@ -0,0 +1,16 @@
+// 從C:\Users\Owner\Local Sites\shuntei\app\public\wp-content\themes\twentytwentytwo-child
+//的"palette": []裡獲取以下顏色
+/*在"settings": {
+     "appearanceTools": true,
+     "color": {
+         "defaultPalette" : false, //加入這一行的話wp的text color的default會消失
+       "duotone": [*/
+export const theme = {
+  foreground: "#000000",
+  background: "#ffffff",
+  primary: "#1a4548",
+  secondary: "#ffe2c7",
+  tertiary: "#1E293B",
+  "vivid-green-cyan": "#00d084",
+  "vivid-red": "#cf2e2e",
+};
